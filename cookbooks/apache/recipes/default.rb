d06ecbc2251af59117ff93f8f61bd33a2a142464
sampath <samnet1984@yahoo.in>
2017-08-14 10:12:11 +0000

    postgress qlo

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -5,7 +5,7 @@
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
 if node['platform_family'] == "rhel"
-	package == "httpd"
+	package = "httpd"
 elsif node['platform_family'] == "debian"
 	package = "apache2"
 end
